ea78b2bc4ed97a6ee19a35a19f498b6317944ac7
AngelesVFA <spotifyasuna21@gmail.com>
2023-07-01 12:12:51 +0200

    itinerary model creation and CRUD routes

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -2,5 +2,6 @@ const router = require("express").Router();
 
 router.use("/", require('./main.routes'))
 router.use("/auth", require("./auth.routes"))
+router.use("/itinerary", require("./itinerary.routes"))
 
 module.exports = router;
