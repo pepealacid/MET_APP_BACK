000328f26a45c8be52b81771f4f55363b8334d15
pepealacid <ppalacid@gmail.com>
2023-07-01 11:54:27 +0200

    artwork controller and crud routes created

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -3,5 +3,6 @@ const router = require("express").Router();
 router.use("/auth", require("./auth.routes"))
 router.use("/", require('./main.routes'))
 router.use("/", require('./user.routes'))
+router.use("/", require("./artwork.routes"))
 
 module.exports = router;
